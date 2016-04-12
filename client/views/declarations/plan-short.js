1aee26a8fe433abf95b30a87aa0e2304fa51104f
itsnhduc <nhduc.lhp@gmail.com>
2016-04-13 02:41:46 +0800

    Add visibility icon to short plan view

--- client/views/declarations/plan-short.js
+++ client/views/declarations/plan-short.js
@@ -1,5 +1,19 @@
 Template.planShort.helpers({
 	'organizer': function() {
+		console.log(this);
 		return getOrganizer(this);
+	},
+	'visibilityIcon': function() {
+		console.log(this);
+		switch(this.visibility) {
+			case 'public':
+				return 'globe'
+			case 'friends':
+				return 'users'
+			case 'private':
+				return 'lock'
+			default:
+				return ''
+		}
 	}
 });
